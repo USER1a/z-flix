fbdd70966fe21d36771e92542b0f79e2943cc944
Anuraag Singh <89829204+Anuraagsingh132@users.noreply.github.com>
2024-10-25 22:14:45 +0530

    Update page.tsx

--- src/app/watch/movie/[slug]/page.tsx
+++ src/app/watch/movie/[slug]/page.tsx
@@ -6,6 +6,6 @@ export const revalidate = 3600;
 export default function Page({ params }: { params: { slug: string } }) {
   const id = params.slug.split('-').pop();
   //return <EmbedPlayer url={`https://embed.su/embed/movie/${id}`} />;
-  return <EmbedPlayer url={`https://vidlink.pro/movie/${id}?primaryColor=${primaryColor}&secondaryColor=${secondaryColor}&iconColor=${iconColor}&autoplay=false`} />;
+  return <EmbedPlayer url={`https://vidlink.pro/movie/${id}`} />;
   
 }
