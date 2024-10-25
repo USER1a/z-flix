4f79dfd86537087365e8420ba7e551e4931e8a69
Anuraag Singh <89829204+Anuraagsingh132@users.noreply.github.com>
2024-10-25 22:08:24 +0530

    Update page.tsx
    change movie server

--- src/app/watch/movie/[slug]/page.tsx
+++ src/app/watch/movie/[slug]/page.tsx
@@ -5,5 +5,7 @@ export const revalidate = 3600;
 
 export default function Page({ params }: { params: { slug: string } }) {
   const id = params.slug.split('-').pop();
-  return <EmbedPlayer url={`https://embed.su/embed/movie/${id}`} />;
+  //return <EmbedPlayer url={`https://embed.su/embed/movie/${id}`} />;
+  return <EmbedPlayer url={`https://vidlink.pro/movie/${mediaId}?primaryColor=${primaryColor}&secondaryColor=${secondaryColor}&iconColor=${iconColor}&autoplay=false`} />;
+  
 }
