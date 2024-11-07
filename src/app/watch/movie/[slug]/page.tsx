d71f0d712194ea47ece809d93428f4e8c964f40f
Anuraag Singh <89829204+Anuraagsingh132@users.noreply.github.com>
2024-11-07 16:02:15 +0530

    Update page.tsx

--- src/app/watch/movie/[slug]/page.tsx
+++ src/app/watch/movie/[slug]/page.tsx
@@ -8,6 +8,7 @@ export default function Page({ params }: { params: { slug: string } }) {
   //return <EmbedPlayer url={`https://embed.su/embed/movie/${id}`} />;
   //return <EmbedPlayer url={`https://vidbinge.dev/embed/movie/${id}`} />;
   //return <EmbedPlayer url={`https://player.vidsrc.nl/embed/movie/${id}`} />;
-  return <EmbedPlayer url={`https://vidsrc.cc/v2/embed/movie/${id}?autoPlay=true`} />;
+  //return <EmbedPlayer url={`https://vidsrc.cc/v2/embed/movie/${id}?autoPlay=true`} />;
+  return <EmbedPlayer url={`https://vidsrc.me/embed/movie/${id}`} />;
   
 }
