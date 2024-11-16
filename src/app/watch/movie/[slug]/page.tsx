13a5bc732fd25430905dbad50403e3dc1ec0e046
Anuraag Singh <89829204+Anuraagsingh132@users.noreply.github.com>
2024-11-16 14:51:18 +0530

    Update page.tsx

--- src/app/watch/movie/[slug]/page.tsx
+++ src/app/watch/movie/[slug]/page.tsx
@@ -6,9 +6,9 @@ export const revalidate = 3600;
 export default function Page({ params }: { params: { slug: string } }) {
   const id = params.slug.split('-').pop();
   //return <EmbedPlayer url={`https://embed.su/embed/movie/${id}`} />;
-  //return <EmbedPlayer url={`https://vidbinge.dev/embed/movie/${id}`} />;
+  return <EmbedPlayer url={`https://vidbinge.dev/embed/movie/${id}`} />;
   //return <EmbedPlayer url={`https://player.vidsrc.nl/embed/movie/${id}`} />;
   //return <EmbedPlayer url={`https://vidsrc.cc/v2/embed/movie/${id}?autoPlay=true`} />;
-  return <EmbedPlayer url={`https://vidsrc.me/embed/movie/${id}?autoPlay=true`} />;
+  //return <EmbedPlayer url={`https://vidsrc.me/embed/movie/${id}?autoPlay=true`} />;
   
 }
