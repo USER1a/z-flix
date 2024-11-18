302a3a6d866bc91ff8be4f7cc796ab5f7afcd5d6
Anuraag Singh <89829204+Anuraagsingh132@users.noreply.github.com>
2024-11-18 15:50:55 +0530

    Update site-header.tsx

--- src/components/main/site-header.tsx
+++ src/components/main/site-header.tsx
@@ -1,28 +1,13 @@
-"use client";
-
 import { siteConfig } from "@/configs/site";
 import React from "react";
 import MainNav from "@/components/navigation/main-nav";
 
 const SiteHeader = () => {
-  // Dynamically load the script on initial render
-  if (typeof window !== "undefined") {
-    const scriptId = "profitable-cpm-script";
-    
-    // Check if the script is already added to avoid duplicates
-    if (!document.getElementById(scriptId)) {
-      const script = document.createElement("script");
-      script.id = scriptId;
-      script.src = "//pl24530115.profitablecpmrate.com/23/5a/d6/235ad6b2de46117f95b93785b3b0198b.js";
-      script.type = "text/javascript";
-      script.async = true;
-      document.body.appendChild(script);
-    }
-  }
-
   return (
+    // <header className="sticky top-0 z-50 border-b bg-background">
     <header className="sticky top-0 z-50">
       <MainNav items={siteConfig.mainNav} />
+      {/* <MobileNav items={siteConfig.mainNav} className="md:hidden" /> */}
     </header>
   );
 };
