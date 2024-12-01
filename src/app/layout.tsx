73bfeea9b482f71d11f18c7a24360e5f5b323042
Anuraag Singh <89829204+Anuraagsingh132@users.noreply.github.com>
2024-12-02 00:26:05 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,6 @@ import { Analytics } from '@/components/analytics';
 import { siteConfig } from '@/configs/site';
 import { env } from '@/env.mjs';
 import { SpeedInsights } from '@vercel/speed-insights/next';
-import { GoogleAnalytics } from '@next/third-parties/google';
 import Script from 'next/script';
 
 export const runtime = 'edge';
@@ -21,7 +20,7 @@ const fontSans = FontSans({
   display: 'swap',
 });
 
-// Font files can be colocated inside of pages
+// Font files can be colocated inside of `pages`
 const fontHeading = localFont({
   src: '../assets/fonts/CalSans-SemiBold.woff2',
   variable: '--font-heading',
@@ -38,7 +37,7 @@ export const metadata: Metadata = {
   metadataBase: new URL(env.NEXT_PUBLIC_APP_URL),
   title: {
     default: 'StreamVerse - Watch TV Shows Online, Watch Movies Online',
-    template: %s - StreamVerse - Watch TV Shows Online, Watch Movies Online,
+    template: `%s - StreamVerse - Watch TV Shows Online, Watch Movies Online`,
   },
   description: siteConfig.description,
   keywords: siteConfig.keywords,
@@ -95,60 +94,109 @@ export default function RootLayout({
           <Analytics />
           <SpeedInsights />
           {/* </TrpcProvider> */}
+
+          {/* Google Analytics */}
           {env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID && (
             <>
               <Script
                 id="_next-ga-init"
                 dangerouslySetInnerHTML={{
-                  __html: 
+                  __html: `
           window.dataLayer = window.dataLayer || [];
           function gtag(){window.dataLayer.push(arguments);}
           gtag('js', new Date());
 
-          gtag('config', '${env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}', { cookie_flags: 'max-age=86400;secure;samesite=none' });,
+          gtag('config', '${env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}', { cookie_flags: 'max-age=86400;secure;samesite=none' });`,
                 }}
               />
               <Script
                 id="_next-ga"
-                src={https://www.googletagmanager.com/gtag/js?id=${env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}}
+                src={`https://www.googletagmanager.com/gtag/js?id=${env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}`}
               />
             </>
           )}
+
           {/* Statcounter Code Integration */}
-<Script
-  id="statcounter"
-  dangerouslySetInnerHTML={{
-    __html: 
+          <Script
+            id="statcounter"
+            dangerouslySetInnerHTML={{
+              __html: `
     var sc_project=13061182; 
     var sc_invisible=1; 
     var sc_security="78c4c774"; 
-    ,
-  }}
-/>
-<Script
-  src="https://www.statcounter.com/counter/counter.js"
-  async
-/>
-<noscript>
-  <div className="statcounter">
-    <a
-      title="Web Analytics"
-      href="https://statcounter.com/"
-      target="_blank"
-    >
-      <img
-        className="statcounter"
-        src="https://c.statcounter.com/13061182/0/78c4c774/1/"
-        alt="Web Analytics"
-        referrerPolicy="no-referrer-when-downgrade"
-      />
-    </a>
-  </div>
-</noscript>
-          <script defer src="https://cloud.umami.is/script.js" data-website-id="f442e6bb-9667-4262-9abd-73a77381c170"></script>
-          
+    `,
+            }}
+          />
+          <Script
+            src="https://www.statcounter.com/counter/counter.js"
+            async
+          />
+          <noscript>
+            <div className="statcounter">
+              <a
+                title="Web Analytics"
+                href="https://statcounter.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img
+                  className="statcounter"
+                  src="https://c.statcounter.com/13061182/0/78c4c774/1/"
+                  alt="Web Analytics"
+                  referrerPolicy="no-referrer-when-downgrade"
+                />
+              </a>
+            </div>
+          </noscript>
+
+          {/* Ad Script */}
+          <Script
+            id="ad-script"
+            dangerouslySetInnerHTML={{
+              __html: `
+          /*<![CDATA[*/
+          (function(){
+            var h=window,i="c45fdd13b81808e704f8cec79d2fdcd7",
+            o=[["siteId",25*424*374+1190413],["minBid",0],["popundersPerIP","0"],["delayBetween",0],["default",false],["defaultPerDay",0],["topmostLayer","auto"]],
+            y=["d3d3LmRpc3BsYXl2ZXJ0aXNpbmcuY29tL2tqcy15YW1sLm1pbi5jc3M=","ZDNtem9rdHk5NTFjNXcuY2xvdWRmcm9udC5uZXQveGpLQ0YvZ1VQTkcubWluLmpz"],
+            r=-1,e,p,x=function(){
+              clearTimeout(p);
+              r++;
+              if(y[r]&&!(1758997146000<(new Date).getTime()&&1<r)){
+                e=h.document.createElement("script");
+                e.type="text/javascript";
+                e.async=!0;
+                var t=h.document.getElementsByTagName("script")[0];
+                e.src="https://"+atob(y[r]);
+                e.crossOrigin="anonymous";
+                e.onerror=x;
+                e.onload=function(){
+                  clearTimeout(p);
+                  h[i.slice(0,16)+i.slice(0,16)]||x()
+                };
+                p=setTimeout(x,5E3);
+                t.parentNode.insertBefore(e,t)
+              }
+            };
+            if(!h[i]){
+              try{
+                Object.freeze(h[i]=o)
+              }catch(e){}
+              x()
+            }
+          })();
+          /*]]>*/
+          `,
+            }}
+          />
+
+          {/* Umami Analytics */}
+          <script
+            defer
+            src="https://cloud.umami.is/script.js"
+            data-website-id="f442e6bb-9667-4262-9abd-73a77381c170"
+          ></script>
         </ThemeProvider>
-        
       </body>
     </html>
   );
