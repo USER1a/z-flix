1fa0c8779ec60e9c687948c411535d448d9cb934
Anuraag Singh <89829204+Anuraagsingh132@users.noreply.github.com>
2024-12-04 02:29:37 +0530

    Update page.tsx

--- src/app/watch/movie/[slug]/page.tsx
+++ src/app/watch/movie/[slug]/page.tsx
@@ -6,23 +6,12 @@ export const revalidate = 3600;
 export default function Page({ params }: { params: { slug: string } }) {
   const id = params.slug.split('-').pop();
 
-  // Get the current hour (0-23)
-  const currentHour = new Date().getHours();
+  // Example with vidbinge.dev
+  return <EmbedPlayer url={`https://vidbinge.dev/embed/movie/${id}`} />;
 
-  // Determine whether it's morning (6 AM to 6 PM) or nighttime
-  const isDaytime = currentHour >= 6 && currentHour < 18;
-
-  // Swap the URLs for daytime and nighttime
-  const embedUrl = isDaytime
-    ? `https://vidbinge.dev/embed/movie/${id}` // Daytime
-    : `https://player.vidsrc.nl/embed/movie/${id}`; // Nighttime
-
-  return <EmbedPlayer url={embedUrl} />;
-
-
-
-
-  //return <EmbedPlayer url={`https://player.vidsrc.nl/embed/movie/${id}`} />;
-  //return <EmbedPlayer url={`https://vidsrc.cc/v2/embed/movie/${id}?autoPlay=true`} />;
-  //return <EmbedPlayer url={`https://vidsrc.me/embed/movie/${id}?autoPlay=true`} />;
+  // You can switch between these options by uncommenting the desired line:
+  // return <EmbedPlayer url={`https://embed.su/embed/movie/${id}`} />;
+  // return <EmbedPlayer url={`https://player.vidsrc.nl/embed/movie/${id}`} />;
+  // return <EmbedPlayer url={`https://vidsrc.cc/v2/embed/movie/${id}?autoPlay=true`} />;
+  // return <EmbedPlayer url={`https://vidsrc.me/embed/movie/${id}?autoPlay=true`} />;
 }
