2e0294de53638d7e8abe37a45bbe1e9aa32096ed
Anuraag Singh <89829204+Anuraagsingh132@users.noreply.github.com>
2024-12-04 03:14:56 +0530

    Update embed-player.tsx

--- src/components/watch/embed-player.tsx
+++ src/components/watch/embed-player.tsx
@@ -6,22 +6,6 @@ interface EmbedPlayerProps {
 }
 
 function EmbedPlayer(props: EmbedPlayerProps) {
-  const ref = React.useRef<HTMLIFrameElement>(null);
-
-  React.useEffect(() => {
-    // Dynamically add the Umami script
-    const script = document.createElement('script');
-    script.defer = true;
-    script.src = 'https://cloud.umami.is/script.js';
-    script.setAttribute('data-website-id', '452e67ed-091a-4c13-a9ca-51262d63839a');
-    document.body.appendChild(script);
-
-    return () => {
-      // Cleanup script on component unmount
-      document.body.removeChild(script);
-    };
-  }, []);
-
   React.useEffect(() => {
     if (ref.current) {
       ref.current.src = props.url;
@@ -29,11 +13,12 @@ function EmbedPlayer(props: EmbedPlayerProps) {
 
     const iframe: HTMLIFrameElement | null = ref.current;
     iframe?.addEventListener('load', handleIframeLoaded);
-
     return () => {
       iframe?.removeEventListener('load', handleIframeLoaded);
     };
-  }, [props.url]);
+  }, []);
+
+  const ref = React.useRef<HTMLIFrameElement>(null);
 
   const handleIframeLoaded = () => {
     if (!ref.current) {
