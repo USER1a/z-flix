5ceee51e802fa5e3125b464daa27dae4ad75449b
Anuraag Singh <89829204+Anuraagsingh132@users.noreply.github.com>
2025-01-01 03:00:53 +0530

    Update next.config.mjs
    redirects to /home

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,9 @@
-/**
- * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
- * for Docker builds.
- */
 const { env } = await import('./src/env.mjs');
 
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: true,
 
-  /**
-   * If you are using `appDir` then you must comment the below `i18n` config out.
-   *
-   * @see https://github.com/vercel/next.js/issues/41980
-   */
   i18n: {
     locales: ['en'],
     defaultLocale: 'en',
@@ -30,6 +21,17 @@ const config = {
     ignoreDuringBuilds: true,
   },
   swcMinify: true,
+
+  // Add the redirects configuration
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/home',
+        permanent: true, // Set to true if this is a permanent redirect (HTTP 308)
+      },
+    ];
+  },
 };
 
 export default config;
