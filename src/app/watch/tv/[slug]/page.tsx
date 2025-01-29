8b1328b5ef8b0bed961463338c26c86a30f987ec
Anuraag Singh <89829204+Anuraagsingh132@users.noreply.github.com>
2025-01-29 15:11:26 +0530

    Update page.tsx

--- src/app/watch/tv/[slug]/page.tsx
+++ src/app/watch/tv/[slug]/page.tsx
@@ -6,7 +6,7 @@ export const revalidate = 3600;
 export default function Page({ params }: { params: { slug: string } }) {
   const id = params.slug.split('-').pop();
  
-  return <EmbedPlayer url={`https://embed.su/embed/tv/${id}/1/1`} />;
-  //return <EmbedPlayer url={`https://vidbinge.dev/embed/tv/${id}/${1}/${1}`} />;
+  //return <EmbedPlayer url={`https://embed.su/embed/tv/${id}/1/1`} />;
+  return <EmbedPlayer url={`https://vidbinge.dev/embed/tv/${id}/${1}/${1}`} />;
   //return <EmbedPlayer url={`  https://flicky.host/embed/tv/${id}/{1}/{1}`} />;
 }
