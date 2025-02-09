32dee0fc917cd1ef6fded1674719b3c6a641d1b3
Anuraag Singh <89829204+Anuraagsingh132@users.noreply.github.com>
2025-02-10 04:25:56 +0530

    Update page.tsx

--- src/app/watch/movie/[slug]/page.tsx
+++ src/app/watch/movie/[slug]/page.tsx
@@ -7,8 +7,8 @@ export default function Page({ params }: { params: { slug: string } }) {
   const id = params.slug.split('-').pop();
 
   // Example with vidbinge.dev
-  return <EmbedPlayer url={`https://flicky.host/embed/movie/?id=${id}`} />;
-  //return <EmbedPlayer url={`https://vidbinge.dev/embed/movie/${id}`} />;
+  //return <EmbedPlayer url={`https://flicky.host/embed/movie/?id=${id}`} />;
+  return <EmbedPlayer url={`https://vidbinge.dev/embed/movie/${id}`} />;
   //return <EmbedPlayer url={`https://vidlink.pro/movie/${id}?primaryColor=63b8bc&secondaryColor=a2a2a2&iconColor=eefdec&icons=vid&player=default&title=true&poster=true&autoplay=true&nextbutton=false`} />;
 
   // You can switch between these options by uncommenting the desired line:
