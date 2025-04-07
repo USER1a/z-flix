3d9ce6c3a8a412a0bbab1be16ede442e4ba84560
Anuraag Singh <89829204+Anuraagsingh132@users.noreply.github.com>
2025-04-08 04:12:32 +0530

    Update page.tsx

--- src/app/watch/movie/[slug]/page.tsx
+++ src/app/watch/movie/[slug]/page.tsx
@@ -7,13 +7,13 @@ export default function Page({ params }: { params: { slug: string } }) {
   const id = params.slug.split('-').pop();
 
   // Example with vidbinge.dev
-  return <EmbedPlayer url={`https://vidsrc.su/embed/movie/${id}`} />;
+  //return <EmbedPlayer url={`https://vidsrc.su/embed/movie/${id}`} />;
   //return <EmbedPlayer url={`https://flicky.host/embed/movie/?id=${id}`} />;
   //return <EmbedPlayer url={`https://vidbinge.dev/embed/movie/${id}`} />;
   //return <EmbedPlayer url={`https://vidlink.pro/movie/${id}?primaryColor=63b8bc&secondaryColor=a2a2a2&iconColor=eefdec&icons=vid&player=default&title=true&poster=true&autoplay=true&nextbutton=false`} />;
 
   // You can switch between these options by uncommenting the desired line:
-  //return <EmbedPlayer url={`https://embed.su/embed/movie/${id}`} />;
+  return <EmbedPlayer url={`https://embed.su/embed/movie/${id}`} />;
   //return <EmbedPlayer url={`https://player.vidsrc.nl/embed/movie/${id}`} />;
   //return <EmbedPlayer url={`https://vidsrc.cc/v2/embed/movie/${id}?autoPlay=true`} />;
   // return <EmbedPlayer url={`https://vidsrc.me/embed/movie/${id}?autoPlay=true`} />;
