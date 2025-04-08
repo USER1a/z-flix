b952d5082537ccebfa5500af1dd4aab9b1ff6504
Anuraag Singh <89829204+Anuraagsingh132@users.noreply.github.com>
2025-04-08 17:01:40 +0530

    Update apiClient.ts

--- src/lib/apiClient.ts
+++ src/lib/apiClient.ts
@@ -1,4 +1,4 @@
 import BaseService from '@/services/BaseService';
 
-const tmdbClient = BaseService.axios(`https://api.themoviedb.org/3/`);
+const tmdbClient = BaseService.axios(`https://tmdb-a62z.onrender.com/api/`);
 export default tmdbClient;
